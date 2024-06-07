cff3279b29a6319c2bde5266625413e01c117f65
Viraj Jagtap <jagtapviraj2003@gmail.com>
2024-06-07 17:50:29 +0530

    ModelClass

--- model_class/lib/view_Employee.dart
+++ model_class/lib/view_Employee.dart
@@ -7,11 +7,12 @@ import 'package:model_class/employee_model.dart';
 class viewEmployee extends StatefulWidget{
   const viewEmployee({super.key});
 
+  @override
   State <viewEmployee> createState() => _viewEmployeeState();
 }
 class _viewEmployeeState extends State<viewEmployee>{
   List<Data> empData=[];
-  
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
@@ -25,7 +26,6 @@ class _viewEmployeeState extends State<viewEmployee>{
           return Row(
            children: [
               Text("${empData[index].employeeName}"),
-              
               const SizedBox(
                 height:40,
               ),
